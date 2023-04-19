cb01348f28973fd05f085caea3fa8dc02325e4d8
dellancelod <danil.svetaylo@gmail.com>
2023-04-19 11:24:13 +0300

    Delete unnesecary comment

--- ServiceChat.cs
+++ ServiceChat.cs
@@ -7,7 +7,6 @@ using System.Text;
 
 namespace WCF_Chat
 {
-    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "ServiceChat" в коде и файле конфигурации.
     public class ServiceChat : IServiceChat
     {
         public void DoWork()
