a80b80059fb18333793554f52db6f2802c8543dd
dellancelod <danil.svetaylo@gmail.com>
2023-04-19 10:51:41 +0300

    Add WCF Service

---
+++ IServiceChat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_Chat
+{
+    // ПРИМЕЧАНИЕ. Можно использовать команду "Переименовать" в меню "Рефакторинг", чтобы изменить имя интерфейса "IServiceChat" в коде и файле конфигурации.
+    [ServiceContract]
+    public interface IServiceChat
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
---
+++ ServiceChat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_Chat
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "ServiceChat" в коде и файле конфигурации.
+    public class ServiceChat : IServiceChat
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
